23d0bc475041f09dc455617f7ad41e2cb8e629b3
rajakumar865465 <rajakumar865465@users.noreply.github.com>
2026-08-21 11:00:08 +0530

    chore: add Meta Pixel tracking code

--- frontend/src/app/layout.tsx
+++ frontend/src/app/layout.tsx
@@ -1,5 +1,6 @@
 import type { Metadata } from "next";
 import { Inter, Poppins } from "next/font/google";
+import Script from "next/script";
 import "./globals.css";
 import { AuthProvider } from "@/contexts/AuthContext";
 
@@ -39,7 +40,26 @@ export default function RootLayout({
 }>) {
   return (
     <html lang="en-IN">
+      <head>
+        <Script id="meta-pixel" strategy="afterInteractive">
+          {`
+            !function(f,b,e,v,n,t,s)
+            {if(f.fbq)return;n=f.fbq=function(){n.callMethod?
+            n.callMethod.apply(n,arguments):n.queue.push(arguments)};
+            if(!f._fbq)f._fbq=n;n.push=n;n.loaded=!0;n.version='2.0';
+            n.queue=[];t=b.createElement(e);t.async=!0;
+            t.src=v;s=b.getElementsByTagName(e)[0];
+            s.parentNode.insertBefore(t,s)}(window, document,'script',
+            'https://connect.facebook.net/en_US/fbevents.js');
+            fbq('init', '1389534106435145');
+            fbq('track', 'PageView');
+          `}
+        </Script>
+      </head>
       <body className={`${inter.variable} ${poppins.variable} antialiased bg-gray-950 text-gray-100 font-sans`} suppressHydrationWarning>
+        <noscript>
+          <img height="1" width="1" style={{ display: "none" }} src="https://www.facebook.com/tr?id=1389534106435145&ev=PageView&noscript=1" alt="" />
+        </noscript>
         <AuthProvider>{children}</AuthProvider>
       </body>
     </html>
